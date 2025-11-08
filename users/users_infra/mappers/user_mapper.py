a6b5634da0b945ebb9172296a499cef9556c6a13
Seungkyu-Han <trust1204@gmail.com>
2025-11-08 11:17:40 +0900

    feat: create user mapper

---
+++ users/users_infra/mappers/user_mapper.py
@@ -0,0 +1,19 @@
+from users.users_core.vo.email import Email
+from users.users_infra.entites.users_entity import UserEntity
+from users.users_core.users import User
+
+def to_domain(user_entity: UserEntity) -> User:
+    return User(
+        user_id=user_entity.user_id,
+        name=user_entity.name,
+        email=Email(user_entity.email),
+        encrypted_password=user_entity.encrypted_password
+    )
+
+def to_entity(user: User) -> UserEntity:
+    return UserEntity(
+        user_id=user.user_id,
+        name=user.name,
+        email=user.email.value,
+        encrypted_password=user.encrypted_password
+    )
