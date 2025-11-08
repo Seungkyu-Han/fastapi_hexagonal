cda0124626fc9f32407f5dd710b3bb7d1b5df16c
Seungkyu-Han <trust1204@gmail.com>
2025-11-08 16:06:18 +0900

    feat: create database.py to use pg database

---
+++ config/database.py
@@ -0,0 +1,43 @@
+import os
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator, Annotated
+
+from dotenv import load_dotenv
+from fastapi import Depends
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER", "user")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "dbname")
+
+DATABASE_URL = (
+    f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+Base = declarative_base()
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session_maker = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False
+)
+
+@asynccontextmanager
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        try:
+            yield session
+        except SQLAlchemyError as e:
+            await session.rollback()
+            raise e
+
+db_session = Annotated[AsyncSession, Depends(get_db_session)]
