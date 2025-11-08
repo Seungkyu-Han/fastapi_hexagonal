e02e1054d47b24d8426a43efbf7d8f43f21a8ad5
Seungkyu-Han <trust1204@gmail.com>
2025-11-08 20:34:06 +0900

    feat: change user id column type

--- users/users_infra/entites/user_entity.py
+++ users/users_infra/entites/user_entity.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, String, DateTime, BigInteger
 
 from config.database import Base
 
@@ -9,7 +9,7 @@ class UserEntity(Base):
 
     __tablename__ = 'users'
 
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True, autoincrement=False)
     name = Column(String(50), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
     encrypted_password = Column(String(255), nullable=False)
